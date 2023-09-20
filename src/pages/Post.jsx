636194ddf93d267bfa9d84a37ec484a8a37c3238
SosaSJuanD <sosasjuandavid@gmail.com>
2023-09-20 12:52:22 -0500

    Solucionando error redireccion con useNavigate para un post que no se encuentra

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
 import "../assets/css/componentes/card.css"
-import { useParams } from "react-router-dom"
+import { useParams, useNavigate } from "react-router-dom"
 import { buscar } from "../api/api"
 
 
@@ -8,9 +8,12 @@ const Post = ({url}) => {
 
     const [post, setpost] = useState([])
     const { id } = useParams()
+    const navigate = useNavigate()
 
     useEffect(()=>{
-        buscar(`/posts/${id}`, setpost)
+        buscar(`/posts/${id}`, setpost).catch(()=>{
+            navigate("/not-found")
+        })
     },[id])
     
     
